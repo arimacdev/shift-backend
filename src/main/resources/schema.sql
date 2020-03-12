15462ef08d1c46784ca658f6ae693fa365243726
Naveen <naveenperera777@gmail.com>
2020-03-12 10:59:35 +0530

    Update database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 06, 2020 at 01:49 PM
+-- Generation Time: Mar 12, 2020 at 05:41 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -34,7 +34,6 @@ CREATE TABLE `Client` (
   `country` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 -- --------------------------------------------------------
 
 --
@@ -61,7 +60,6 @@ CREATE TABLE `ProjectRole` (
   `projectRoleName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 -- --------------------------------------------------------
 
 --
@@ -73,9 +71,56 @@ CREATE TABLE `Project_User` (
   `assigneeId` varchar(255) NOT NULL,
   `assignedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `assigneeJobRole` varchar(255) NOT NULL,
-  `assigneeProjectRole` int(11) NOT NULL
+  `assigneeProjectRole` int(11) NOT NULL,
+  `isBlocked` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SubTask`
+--
+
+CREATE TABLE `SubTask` (
+  `subtaskId` varchar(255) NOT NULL,
+  `taskId` varchar(255) NOT NULL,
+  `subtaskName` varchar(255) NOT NULL,
+  `subtaskStatus` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Task`
+--
+
+CREATE TABLE `Task` (
+  `taskId` varchar(255) NOT NULL,
+  `projectId` varchar(255) NOT NULL,
+  `taskName` varchar(255) NOT NULL,
+  `taskInitiator` varchar(255) NOT NULL,
+  `taskAssignee` varchar(255) NOT NULL,
+  `taskNote` varchar(255) DEFAULT NULL,
+  `taskStatus` varchar(255) NOT NULL,
+  `taskCreatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `taskDueDateAt` timestamp NULL DEFAULT NULL,
+  `taskReminderAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TaskFile`
+--
+
+CREATE TABLE `TaskFile` (
+  `taskFileId` varchar(255) NOT NULL,
+  `taskId` varchar(255) NOT NULL,
+  `taskFileName` varchar(255) NOT NULL,
+  `taskFileUrl` varchar(255) NOT NULL,
+  `fileCreatortaskFileCreator` varchar(255) NOT NULL,
+  `taskFileDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -90,7 +135,6 @@ CREATE TABLE `User` (
   `email` varchar(255) NOT NULL,
   `idpUserId` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 --
 -- Indexes for dumped tables
@@ -119,6 +163,24 @@ ALTER TABLE `ProjectRole`
 --
 ALTER TABLE `Project_User`
   ADD PRIMARY KEY (`projectId`,`assigneeId`);
+
+--
+-- Indexes for table `SubTask`
+--
+ALTER TABLE `SubTask`
+  ADD PRIMARY KEY (`subtaskId`);
+
+--
+-- Indexes for table `Task`
+--
+ALTER TABLE `Task`
+  ADD PRIMARY KEY (`taskId`);
+
+--
+-- Indexes for table `TaskFile`
+--
+ALTER TABLE `TaskFile`
+  ADD PRIMARY KEY (`taskFileId`);
 
 --
 -- Indexes for table `User`
