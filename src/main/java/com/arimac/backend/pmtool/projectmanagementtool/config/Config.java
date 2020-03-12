ddfd9c2f53387b43685e9539a43c320e5b57ac28
Naveen <naveenperera777@gmail.com>
2020-03-12 14:44:39 +0530

    disable auth config

--- src/main/java/com/arimac/backend/pmtool/projectmanagementtool/config/Config.java
+++ src/main/java/com/arimac/backend/pmtool/projectmanagementtool/config/Config.java
@@ -1,6 +1,6 @@
 package com.arimac.backend.pmtool.projectmanagementtool.config;
 
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+//import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,10 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
-    @Bean
-    public KeycloakClientRequestFactory keyConfig(){
-        return new KeycloakClientRequestFactory();
-    }
+//    @Bean
+//    public KeycloakClientRequestFactory keyConfig(){
+//        return new KeycloakClientRequestFactory();
+//    }
 
     @Bean
     public RestTemplate restTemplate() {
