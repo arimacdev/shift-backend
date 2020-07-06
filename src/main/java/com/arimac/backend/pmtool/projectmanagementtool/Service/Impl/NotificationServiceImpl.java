97fcc41149700bef49ba74dc80e25054ce640f98
Naveen <naveenperera777@gmail.com>
2020-07-06 16:39:24 +0530

    User Mention Slack Notification

--- src/main/java/com/arimac/backend/pmtool/projectmanagementtool/Service/Impl/NotificationServiceImpl.java
+++ src/main/java/com/arimac/backend/pmtool/projectmanagementtool/Service/Impl/NotificationServiceImpl.java
@@ -665,32 +665,78 @@ public class NotificationServiceImpl implements NotificationService {
         Comment comment = commentRepository.getCommentById(mentionDto.getCommentId());
         if (comment == null)
             return new ErrorMessage(ResponseMessage.COMMENT_NOT_FOUND, HttpStatus.NOT_FOUND);
-//        if (!comment.getEntityId().equals(mentionDto.getEntityId()))
-//            return new ErrorMessage(ResponseMessage.COMMENT_NOT_FOUND, Ht)
-        for (User activeUser: activeUsers){
-           List<UserNotification> oneSignalDevices = userNotificationRepository.getNotificationUserByProviderAndStatus(activeUser.getUserId(), NotificationEnum.OneSignal.toString(), true);
-            if (!oneSignalDevices.isEmpty()) {
-                for (UserNotification device: oneSignalDevices){
-                    StringBuilder oneSignalMentionNotf = getTaskOneSignalMessage(activeUser, task, project, OneSignalMessages.COMMENT_MENTION);
-                    oneSignalMentionNotf.append(OneSignalMessages.COMMENT);
-                    oneSignalMentionNotf.append(comment.getContent());
-                    oneSignalMentionNotf.append(OneSignalMessages.MENTIONED_BY);
-                    oneSignalMentionNotf.append(sender.getFirstName());
-                    oneSignalMentionNotf.append(" ");
-                    oneSignalMentionNotf.append(sender.getLastName());
+        for (User activeUser: activeUsers) {
+            if (projectRepository.getProjectUser(project.getProjectId(), activeUser.getUserId()) != null) {
+                List<UserNotification> oneSignalDevices = userNotificationRepository.getNotificationUserByProviderAndStatus(activeUser.getUserId(), NotificationEnum.OneSignal.toString(), true);
+                if (!oneSignalDevices.isEmpty()) {
+                    for (UserNotification device : oneSignalDevices) {
+                        StringBuilder oneSignalMentionNotf = getTaskOneSignalMessage(activeUser, task, project, OneSignalMessages.COMMENT_MENTION);
+                        oneSignalMentionNotf.append(OneSignalMessages.COMMENT);
+                        oneSignalMentionNotf.append(comment.getContent());
+                        oneSignalMentionNotf.append(OneSignalMessages.MENTIONED_BY);
+                        oneSignalMentionNotf.append(sender.getFirstName());
+                        oneSignalMentionNotf.append(" ");
+                        oneSignalMentionNotf.append(sender.getLastName());
 
-                    if (device.getPlatform().equals(NotificationPlatformEnum.Mobile.toString()))
-                        sendOneSignalMobileNotification(oneSignalMentionNotf.toString(), device.getSubscriptionId(), "TasksDetailsScreen", project.getProjectId(), project.getProjectName(), task.getTaskId());
-                    else
-                        sendOneSignalNotification(oneSignalMentionNotf.toString(), device.getSubscriptionId());
+                        if (device.getPlatform().equals(NotificationPlatformEnum.Mobile.toString()))
+                            sendOneSignalMobileNotification(oneSignalMentionNotf.toString(), device.getSubscriptionId(), "TasksDetailsScreen", project.getProjectId(), project.getProjectName(), task.getTaskId());
+                        else
+                            sendOneSignalNotification(oneSignalMentionNotf.toString(), device.getSubscriptionId());
+                    }
                 }
+                if (activeUser.getUserSlackId() != null && activeUser.getNotification()) {
+                    JSONObject payload = new JSONObject();
+                    payload.put(CHANNEL, activeUser.getUserSlackId());
+                    payload.put(TEXT, SlackMessages.COMMENT_MENTION);
+                    List<SlackBlock> blocks = new ArrayList<>();
+                    SlackBlock headerBlock = new SlackBlock();
+                    headerBlock.setType(SECTION);
+                    headerBlock.getText().setType(MARK_DOWN);
+                    StringBuilder greeting = new StringBuilder();
+                    greeting.append(getWelcomeAddressing(activeUser.getUserSlackId()));
+                    greeting.append(SlackMessages.COMMENT_MENTION);
+                    headerBlock.getText().setText(greeting.toString());
+                    headerBlock.setAccessory(null);
+                    headerBlock.setElements(null);
+                    blocks.add(headerBlock);
+
+                    SlackBlock divider = new SlackBlock();
+                    divider.setType(DIVIDER);
+                    divider.setText(null);
+                    divider.setAccessory(null);
+                    blocks.add(divider);
+
+                    SlackBlock body = new SlackBlock();
+                    body.setType(SECTION);
+                    body.getText().setType(MARK_DOWN);
+                    StringBuilder bodyText = new StringBuilder();
+                    bodyText.append(SlackMessages.COMMENT);
+                    bodyText.append(comment.getContent());
+                    bodyText.append(SlackMessages.TASK_ICON);
+                    bodyText.append(getTaskUrl(task));
+                    bodyText.append(SlackMessages.PROJECT_ICON);
+                    bodyText.append(getProjectUrl(project));
+
+                    body.getText().setText(bodyText.toString());
+                    body.getAccessory().setType("image");
+                    body.getAccessory().setImage_url(SlackMessages.ASSIGNMENT_THUMBNAIL);
+                    body.getAccessory().setAlt_text("Comment Thumbnail");
+                    blocks.add(body);
+                    blocks.add(getFooter(task.getTaskStatus().toString()));
+                    blocks.add(divider);
+
+                    payload.put(BLOCKS, blocks);
+                    StringBuilder url = new StringBuilder();
+                    url.append(ENVConfig.SLACK_BASE_URL);
+                    url.append("/chat.postMessage");
+                    logger.info("Slack Message Url {}", url);
+                    HttpEntity<Object> entity = new HttpEntity<>(payload.toString(), getHttpHeaders());
+                    Object response = restTemplate.exchange(url.toString(), HttpMethod.POST, entity, String.class);
+                }
+
             }
-
         }
-
         return new Response(ResponseMessage.SUCCESS, HttpStatus.OK);
-
-            
 
     }
 
