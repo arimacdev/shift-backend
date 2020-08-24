1ef029303204567131c3fe80c5cb178e5d0b663f
Naveen <naveenperera777@gmail.com>
2020-08-24 20:45:58 +0530

    Project Overview Endpoint Modification

--- src/main/java/com/arimac/backend/pmtool/projectmanagementtool/Service/Impl/AnalyticsServiceImpl.java
+++ src/main/java/com/arimac/backend/pmtool/projectmanagementtool/Service/Impl/AnalyticsServiceImpl.java
@@ -13,7 +13,6 @@ import com.arimac.backend.pmtool.projectmanagementtool.model.User;
 import com.arimac.backend.pmtool.projectmanagementtool.repository.ProjectRepository;
 import com.arimac.backend.pmtool.projectmanagementtool.repository.TaskRepository;
 import com.arimac.backend.pmtool.projectmanagementtool.repository.UserRepository;
-import org.joda.time.Days;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -27,7 +26,6 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class AnalyticsServiceImpl implements AnalyticsService {
@@ -39,6 +37,8 @@ public class AnalyticsServiceImpl implements AnalyticsService {
     private final ProjectRepository projectRepository;
     private final TaskRepository taskRepository;
     private int dateCount = 0;
+    private String previousFromDate = null;
+    private String previousToDate = null;
 
     public AnalyticsServiceImpl(UserRepository userRepository, ProjectRepository projectRepository, TaskRepository taskRepository) {
         this.userRepository = userRepository;
@@ -76,30 +76,25 @@ public class AnalyticsServiceImpl implements AnalyticsService {
         if (error instanceof ErrorMessage)
             return error;
         int projectCountGiven = projectRepository.getActiveProjectCount(from, to);
-        int projectCountTotal = projectRepository.getActiveProjectCount(ALL,ALL);
-        List<ProjectStatusCountDto> projectStatusCountGiven = projectRepository.getActiveProjectCountByStatus(from, to);
-        List<ProjectStatusCountDto> projectStatusCountTotal = projectRepository.getActiveProjectCountByStatus(ALL, ALL);
+        int projectCountTotal = projectRepository.getActiveProjectCount(previousFromDate,previousToDate);
+//        List<ProjectStatusCountDto> projectStatusCountGiven = projectRepository.getActiveProjectCountByStatus(from, to);
+//        List<ProjectStatusCountDto> projectStatusCountTotal = projectRepository.getActiveProjectCountByStatus(ALL, ALL);
 
-        ProjectStatusCountDto presalesStage = projectStatusCountGiven
-                .stream().filter(project -> Arrays.asList(
-                        "presales",
-                        "presalesPD",
-                        "preSalesQS",
-                        "preSalesN",
-                        "preSalesC",
-                        "preSalesL")
-                .contains(project.getProjectStatus().toString()))
-                .findAny().orElse(null);
+        ProjectStatusCountDto presalesStageGiven = this.getPreSalesProjectStatusCount(projectRepository.getActiveProjectCountByStatus(from, to));
+        ProjectStatusCountDto presalesStageTotal = this.getPreSalesProjectStatusCount(projectRepository.getActiveProjectCountByStatus(previousFromDate, previousToDate));
 
-        //ProjectOverViewDto projectOverView = new ProjectOverViewDto();
-
-//        AspectSummary projectOverView = new AspectSummary();
-//        projectOverView.setDays(presalesStage.getProjectCount());
-//        projectOverView.setDays();
-//
-//
-//        projectOverView.setTotalProjects();
-//
+        ProjectOverViewDto projectOverView = new ProjectOverViewDto();
+        projectOverView.setTotalProjects(getAspectSummary(projectCountGiven, projectCountTotal));
+        if (presalesStageGiven!= null && presalesStageTotal != null){
+            projectOverView.setLeadsPending(getAspectSummary(presalesStageGiven.getProjectCount(), presalesStageTotal.getProjectCount()));
+            projectOverView.setLeadsOngoing(getAspectSummary((projectCountGiven - presalesStageGiven.getProjectCount()), (projectCountTotal - presalesStageTotal.getProjectCount())));
+            projectOverView.setLeadConversion(getConversionPercentage((projectCountGiven - presalesStageGiven.getProjectCount()), projectCountGiven, (projectCountTotal - presalesStageTotal.getProjectCount()), projectCountTotal ));
+        } else {
+            projectOverView.setLeadsPending(null);
+            projectOverView.setLeadsOngoing(new AspectSummary<Integer>(projectCountGiven, this.dateCount, PerformanceEnum.neutral, new BigDecimal("0.00")));
+        }
+//        projectOverView.setLeadsOngoing(getAspectSummary(projectCountGiven));
+//        projectOverView.setLeadsOngoing(getAspectSummary(presalesStageGiven.setProjectCount(projectOverView.getTotalProjects().getValue() - presalesStageGiven.getProjectCount()), presalesStageTotal));
 //
 //        projectOverView.setTotalProjects(projectCount);
 //        if (presalesStage != null) {
@@ -113,38 +108,48 @@ public class AnalyticsServiceImpl implements AnalyticsService {
 //            projectOverView.setLeadConversion(new BigDecimal("100.00"));
 //        }
 
-        return new Response(ResponseMessage.SUCCESS, null);
+        return new Response(ResponseMessage.SUCCESS, HttpStatus.OK, projectOverView);
     }
 
-//    private ProjectOverViewDto getProjectOverView(ProjectStatusCountDto preSalesCountGiven, ProjectStatusCountDto preSalesCountTotal){
-//        if (projectStatusCount!= null){
-//            AspectSummary aspectSummary = new AspectSummary();
-//            aspectSummary.setDays(dateCount);
-//            aspectSummary.setValue(projectStatusCount.getProjectCount());
-//            aspectSummary.setPercentage();
-//            aspectSummary.setPerformance();
-//        } else {
-//
-//        }
-//    }
 
-    private AspectSummary getAspectSummary(ProjectStatusCountDto preSalesCountGiven, ProjectStatusCountDto preSalesCountTotal){
-        AspectSummary aspectSummary = new AspectSummary();
-        if (preSalesCountGiven != null &&  preSalesCountTotal != null){
-            aspectSummary.setValue(preSalesCountGiven.getProjectCount());
-            aspectSummary.setDays(this.dateCount);
-            aspectSummary.setPercentage(getPercentage(preSalesCountGiven.getProjectCount(), preSalesCountTotal.getProjectCount()));
+    private AspectSummary<Integer> getAspectSummary(int given, int total){
+        AspectSummary<Integer> aspectSummary = new AspectSummary<>(given);
+        aspectSummary.setDays(this.dateCount);
+//        if (given != null &&  preSalesCountTotal != null){
+            aspectSummary.setValue(given);
+            aspectSummary.setPercentage(getPercentage(given,total));
             if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) > 0){
                 aspectSummary.setPerformance(PerformanceEnum.increase);
-            } else if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) < 0){
+            } else if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) < 0){ // not invoked
                 aspectSummary.setPerformance(PerformanceEnum.decrease);
             } else if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) == 0){
-                aspectSummary.setPerformance(PerformanceEnum.neutral);            }
-        } else {
+                aspectSummary.setPerformance(PerformanceEnum.neutral);
+            }
+//        } else {
+//            aspectSummary.setPerformance(PerformanceEnum.neutral);
+//        }
+        return aspectSummary;
+    }
 
+    private AspectSummary<BigDecimal> getConversionPercentage(int givenOngoing, int givenTotalProjects, int previousOngoing, int previousTotalProjects){
+        AspectSummary<BigDecimal> aspectSummary = new AspectSummary<>();
+        aspectSummary.setDays(this.dateCount);
+        MathContext mc = new MathContext(2);
+        BigDecimal givenRate = getPercentage(givenOngoing, givenTotalProjects);
+        BigDecimal previousRate = getPercentage(previousOngoing, previousTotalProjects);
+        BigDecimal performanceRate = givenRate.divide(previousRate, mc).multiply( new BigDecimal(100));
+        aspectSummary.setValue(givenRate);
+        aspectSummary.setPercentage(performanceRate);
+        if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) > 0){
+            aspectSummary.setPerformance(PerformanceEnum.increase);
+        } else if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) < 0){ // not invoked
+            aspectSummary.setPerformance(PerformanceEnum.decrease);
+        } else if (aspectSummary.getPercentage().compareTo(BigDecimal.ZERO) == 0){
+            aspectSummary.setPerformance(PerformanceEnum.neutral);
         }
-        aspectSummary.setValue(preSalesCountGiven.getProjectCount());
-        return null;
+
+        return aspectSummary;
+
     }
 
     private ProjectStatusCountDto getPreSalesProjectStatusCount(List<ProjectStatusCountDto> projectCountList){
@@ -161,8 +166,10 @@ public class AnalyticsServiceImpl implements AnalyticsService {
     }
 
     private BigDecimal getPercentage(int value, int total){
+        if (total == 0)
+            return new BigDecimal("0.00");
         MathContext mc = new MathContext(2);
-         return BigDecimal.valueOf(value).divide(BigDecimal.valueOf(total), mc).multiply(new BigDecimal(100));
+         return BigDecimal.valueOf(value - total).divide(BigDecimal.valueOf(total), mc).multiply(new BigDecimal(100));
     }
 
     private Object dateCheck(String from, String to){
@@ -175,11 +182,14 @@ public class AnalyticsServiceImpl implements AnalyticsService {
                 toDate = dateFormat.parse(to);
                 if (fromDate.after(toDate) || toDate.before(fromDate))
                     return new ErrorMessage(ResponseMessage.INVALID_DATE_FORMAT, HttpStatus.BAD_REQUEST);
-                this.dateCount = (int)( (fromDate.getTime() - toDate.getTime()) / (1000 * 60 * 60 * 24));
-                return null;
+                this.dateCount = (int)( (toDate.getTime() - fromDate.getTime()) / (1000 * 60 * 60 * 24));
+                this.previousFromDate = (new Date(fromDate.getTime() - this.dateCount*(1000*60*60*24))).toString();
+                this.previousToDate = (new Date(toDate.getTime() - this.dateCount*(1000*60*60*24))).toString();
+            return null;
             } catch (ParseException e) {
                 return new ErrorMessage(ResponseMessage.INVALID_DATE_FORMAT, HttpStatus.BAD_REQUEST);
             }
         } else return null;
     }
+
 }
