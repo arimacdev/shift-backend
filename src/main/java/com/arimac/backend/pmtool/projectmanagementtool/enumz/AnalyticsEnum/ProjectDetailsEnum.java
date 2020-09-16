685f09c4a9b3686667fb20a73039471ba3a310c7
Naveen <naveenperera@gitlab.com>
2020-09-16 21:07:19 +0530

    Fix Issue of Loading Less Records

--- src/main/java/com/arimac/backend/pmtool/projectmanagementtool/enumz/AnalyticsEnum/ProjectDetailsEnum.java
+++ src/main/java/com/arimac/backend/pmtool/projectmanagementtool/enumz/AnalyticsEnum/ProjectDetailsEnum.java
@@ -1,10 +1,27 @@
 package com.arimac.backend.pmtool.projectmanagementtool.enumz.AnalyticsEnum;
 
+import com.arimac.backend.pmtool.projectmanagementtool.enumz.ProjectStatusEnum;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public enum ProjectDetailsEnum {
     projectName,
-    projectCreatedDate,
+    projectStartDate,
     projectStatus,
     taskcount,
     memberCount,
-    timeTaken
+    timeTaken;
+
+    private static final Set<String> values = new HashSet<>();
+
+    static {
+        for (ProjectDetailsEnum detailsEnum: ProjectDetailsEnum.values()){
+            values.add(detailsEnum.name());
+        }
+    }
+
+    public static boolean contains(String value){
+        return  values.contains(value);
+    }
 }
